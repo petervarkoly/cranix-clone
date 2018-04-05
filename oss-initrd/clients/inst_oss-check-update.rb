e53ef54673b45a0b9ee5da34877046e40830fe2c
Peter Varkoly <varkoly@suse.de>
2018-04-05 15:56:03 +0200

    Show warning by upgrade.

--- oss-initrd/clients/inst_oss-check-update.rb
+++ oss-initrd/clients/inst_oss-check-update.rb
@@ -14,6 +14,14 @@ module Yast
                                 "You can not continue the migration. Restart the old OSS system an start the migration script or contact support@extis.de.<br>"
                                 )
 		return :abort
+	else
+		Popup.LongMessage("Bitte beachten Sie für die Migration: auch wenn der Prozeß sorgfältig getestet wurde, können wir keine Garantie für die Übernahme aller Daten übernehmen. " +
+				"Der Migrations-Prozeß kann nur Daten übernehmen, die von einem richtig gepflegten und aktualisierten OSS 3 stammen.<br>" +
+				"Daten von installierter Fremdsoftware oder OSS Daten, die von Fremdsoftware angepaßt und angereichert wurden, können ggf. nicht korrekt übernommen werden. " +
+				"Wenn Sie in Ihrem OSS Fremdsoftware installiert haben, dann wird diese aller Voraussicht nach deinstalliert oder wird nicht mehr korrekt funktionieren.<br><br>" +
+				"Aus Umstellung zur Active-Directory Server Kompatibilität und der daraus resultierenden Notwendigkeit der Eindeutigkeit von Objekt-Namen ergibt sich die Besonderheit, " +
+				"dass bei Benutzer, Gruppen und Workstation-Accounts mit identischen Namen nur das Benutzerkonto migriert wird. " +
+				"Die Dateien der Gruppe bleiben dann zwar erhalten, die Gruppe selbst wird jedoch nicht migriert. ")
 	end
         :next
     end
