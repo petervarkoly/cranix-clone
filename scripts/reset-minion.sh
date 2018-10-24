e2bb2cde0838da6b654d132d8d0de85a3d67d366
Peter Varkoly <peter@varkoly.de>
2018-10-24 17:26:17 +0200

    Ticket#2018102467001189 — Bitte um die Aufnahme von /usr/bin/chroot aus coreutils in die initrd des Clonetools

---
+++ scripts/reset-minion.sh
@@ -0,0 +1,27 @@
+SERVER=install
+mkdir /mnt/sda{1,2}
+mount /dev/sda1 /mnt/sda1
+mount /dev/sda2 /mnt/sda2
+echo "username=ossreader" >  /tmp/credentials
+echo "password=ossreader" >> /tmp/credentials
+echo "ossreader" > /tmp/username
+echo "ossreader" > /tmp/userpassword
+chmod 400 /tmp/userpassword
+chmod 400 /tmp/credentials
+. /tmp/credentials
+TOKEN=$( curl --insecure -X POST --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: text/plain' -d "username=$username&password=$password" 'https://admin/api/sessions/login' )
+HOSTNAME=$( curl --insecure -X GET --header 'Accept: text/plain' --header "Authorization: Bearer $TOKEN" 'https://admin/api/sessions/dnsName' )
+DOMAIN=$( curl --insecure -X GET --header 'Accept: text/plain' --header "Authorization: Bearer $TOKEN" 'https://admin/api/sessions/domainName' )
+echo "TOKEN=$TOKEN"       >  /tmp/apiparams
+echo "HOSTNAME=$HOSTNAME" >> /tmp/apiparams
+. /tmp/apiparams
+sed -i "s/^id:.*/id: ${HOSTNAME}.${DOMAIN}/" /mnt/sda2/salt/conf/minion
+rm -f /mnt/sda2/salt/conf/pki/minion/*
+rm -r /mnt/sda2/scripts
+rm -r /mnt/sda1/scripts
+curl --insecure -X PUT --header 'Accept: text/plain' --header "Authorization: Bearer $TOKEN" 'https://admin/api/clonetool/resetMinion'
+grep id: /mnt/sda2/salt/conf/minion
+umount /dev/sda1
+umount /dev/sda2
+ps aux | grep start
+
