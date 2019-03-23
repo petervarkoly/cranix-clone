6d435d1e240e98ae79a12cece527586a39335556
Peter Varkoly <varkoly@suse.com>
2019-03-23 09:18:33 +0100

    Merge OSS-4-0 now it is the master.

--- config/domainjoin.bat
+++
@@ -1,8 +0,0 @@
-:answer0 echo " 0 "
-ping admin > NULL
-if %errorlevel% NEQ 0 goto answer0
-
-powershell -noprofile -executionpolicy bypass -file %systemdrive%\script\domainjoin.ps1 -mode domainjoin
-
-ping -n 10 admin > NULL
-
